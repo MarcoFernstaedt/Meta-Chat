1f5bdb3494d85942cc69438a81f509e16733cca8
Marrco Fernstaedt <marco@Marrcos-MacBook-Pro.local>
2021-12-20 10:45:44 -0700

    Meta Chat Complete

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { useMoralis } from "react-moralis";
 
-const Avatar = (username, logoutOnPress) => {
+const Avatar = ({ username, logoutOnPress }) => {
     const { user, logout } = useMoralis();
 
     return <Image className="rounded-full bg-black cursor-pointer hover:opacity-75" src={`https://avatars.dicebear.com/api/pixel-art/${username || user.get("username")}.svg`} onClick={() => logoutOnPress && logout()} layout="fill" />
